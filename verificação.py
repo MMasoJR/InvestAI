c8faf3999f3ef5d019095e348314e1b375d0287d
Maso <marcelomasojunior@gmail.com>
2026-07-02 12:29:11 -0300

    Delete verificação.py

--- verificação.py
+++
@@ -1,7 +0,0 @@
-from processing.chunking.cvm_parecer_chunker import *
-import pandas as pd
-df = pd.read_parquet("data/processed/cvm/dfp/2023/dfp_cia_aberta_parecer_2023.parquet")
-col = detect_text_column(df)
-print(col)                    # deve ser algo como "TX_PARECER" ou similar
-print(df[col].iloc[0][:200]) # deve parecer início de relatório de auditoria
-exit()
